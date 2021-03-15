2088c63e45200380f358a573722989033b47d147
kingsman142 <kingsman142@yahoo.com>
2021-03-15 16:12:24 -0400

    added limit to search function, fixed imports, fixed tfidf, fixed assignment of posts_tokens, added debugging statements

--- utils/calculations.py
+++ utils/calculations.py
@@ -1,16 +1,17 @@
 import numpy as np
+
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
 
-import constants
+import utils.constants as constants
 
 def calc_tfidf(posts_tokens):
     # construct the list of unique tokens (i.e. vocabulary)
     vocabulary = set()
     for post_tokens in posts_tokens:
         unique_tokens = set(post_tokens)
-        vocabulary.intersection(unique_tokens) # create a superset of the tokens across the posts
+        vocabulary = vocabulary.union(unique_tokens) # create a superset of the tokens across the posts
     vocabulary = list(vocabulary) # must convert from a set to list for the CountVectorizer below
 
     # join all the tokens in a post and separate by whitespace (required for the fit function below)
@@ -20,6 +21,6 @@ def calc_tfidf(posts_tokens):
     pipe = Pipeline([('count', CountVectorizer(vocabulary = vocabulary)), ('tfidf', TfidfTransformer())]).fit(posts_content)
 
     # sort vocab and tfidf scores together (sort in descending order of tfidf score)
-    vocab_words, vocab_tfidf = zip(*sorted(zip(vocabulary, pipe['tfidf'].idf_), key=itemgetter(1), reverse = True))[:constants.TOP_N_KEYWORDS]
-    
-    return vocab_words, vocab_tfidf
+    vocab_words, vocab_tfidf = zip(*sorted(zip(vocabulary, pipe['tfidf'].idf_), key = lambda x : x[1], reverse = True))
+
+    return vocab_words[:constants.TOP_N_KEYWORDS], vocab_tfidf[:constants.TOP_N_KEYWORDS]
